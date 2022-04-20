32c3762a6854426f1d1b7f4b06f5e1402ec9d8ad
Omar <omarhlode@gmail.com>
2022-04-20 12:55:14 +0100

    añadir lista y links en nombres

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ btRequest.addEventListener('click', () => {
 onRequest()
 })
 
+/* funcion sincrona (recordar el llamada/nombre Id de cada elemento) 
 function onRequest() {
 fetch('https://jsonplaceholder.typicode.com/users')
 .then(response => {
@@ -14,21 +15,21 @@ fetch('https://jsonplaceholder.typicode.com/users')
     return response.json()
     })
 
-.then(data => {
+.then(data => { */
 /*  console.log(data)
-    console.log(data[0].id + " - "+ data[0].name) */
+    console.log(data[0].id + " - "+ data[0].name)
     for (const user of data){
         console.log(user.id + " - "+ user.name)
     }
-})
+}) */
 //solo en el caso de sacar objeto en arrays
 
-.catch(error => {
+/* .catch(error => {
     console.log(error)
 });
-}
+} */
 
-async function onRequestasync() {
+ async function onRequest() {
     try {
         const response = await fetch('https://jsonplaceholder.typicode.com/users');
         if (!response.ok) {
@@ -40,10 +41,26 @@ async function onRequestasync() {
     } catch (error) {
         console.log(error)
     }
-}
+} 
 function printData(data){
+    const lista=document.getElementById("lista")
+
     for (const user of data){
-        console.log(user.id + " - "+ user.name)
+        
+        //console.log(user.id + " - "+ user.name)
+        
+        const li=document.createElement("li")
+        const a=document.createElement("a")
+        
+        a.textContent= user.name //usamos la variable user que para algo esta, junto con lo que queremos definir
+        //concat1
+        //a.href=`http://${user.website}`
+        //concat2
+        a.href="http://" + user.website
+
+        li.appendChild(a)
+        lista.appendChild(li)
+        
     }
 
 }
