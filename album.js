511b83cbe674284c7fab2d5be1250e05dbd8ede6
Omar <omarhlode@gmail.com>
2022-04-28 10:09:53 +0100

    Forma3 añadida, 2 fetch 1 fuction

--- album.js
+++ album.js
@@ -72,6 +72,7 @@ function printData(albums, users) {
   }
 }
 
+//forma2
 /* async function requestAlbums(users) {
   try {
       const response = await fetch('https://jsonplaceholder.typicode.com/albums');
@@ -120,3 +121,47 @@ function showData(albums, users) {
   }
 }
  */
+//forma 3
+/*async function requestAlbumsWithUsers() {
+  try {
+      const responseUsers = await fetch('https://jsonplaceholder.typicode.com/users');
+      if (!responseUsers.ok) {
+          const message = `Error: ${response.status}`;
+          throw new Error(message);
+      }
+
+      const users = await responseUsers.json();
+
+
+      const responseAlbums = await fetch('https://jsonplaceholder.typicode.com/albums');
+      if (!responseAlbums.ok) {
+          const message = `Error: ${response.status}`;
+          throw new Error(message);
+      }
+      const albums = await responseAlbums.json();
+
+      showData(albums, users)
+  } catch (error) {
+      console.log(error)
+  }
+}
+
+requestAlbumsWithUsers()
+
+function showData(albums, users) {
+  albums.sort(() => Math.random() - 0.5);
+  const lista = document.getElementById("lista")
+  for (const album of albums) {
+
+      const user = users.find(newUser => newUser.id == album.userId);
+
+      const title = document.createElement("h2")
+      const userName = document.createElement("p")
+
+      title.textContent = album.title
+      userName.textContent = user.name
+
+      lista.appendChild(title)
+      lista.appendChild(userName)
+  }
+}*/
