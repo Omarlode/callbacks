ef2a8deff70c0612085419138ad0eb068e46f041
Omar <omarhlode@gmail.com>
2022-04-28 10:04:07 +0100

    caso 1 y 2 de doblepeticion y printeado

--- album.js
+++ album.js
@@ -1,52 +1,43 @@
 onRequest();
-//onRequestuser();
-/* async function onRequestuser() {
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    if (!response.ok) {
-      const message = `Error: ${response.status}`;
-      throw new Error(message);
-    }
-    const users2 = await response.json();
-    printName(users2);
-  } catch (error) {
-    console.log(error);
-  }
-} */
+
 async function onRequest() {
   try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/albums");
-    if (!response.ok) {
-      const message = `Error: ${response.status}`;
-      throw new Error(message);
-    }
-    const albums = await response.json();
-    printData(albums);
+    const response = await Promise.all([
+      fetch("https://jsonplaceholder.typicode.com/albums").then((response) => {
+        if (!response.ok) {
+          const message = `Error: ${response.status}`;
+          throw new Error(message);
+        }
+        return response.json();
+      }),
+
+      fetch("https://jsonplaceholder.typicode.com/users").then((response) => {
+        if (!response.ok) {
+          const message = `Error: ${response.status}`;
+          throw new Error(message);
+        }
+        return response.json();
+      }),
+    ]);
+
+    let albums = response[0];
+    let users = response[1];
+    //console.log(albums);
+    //console.log(users);
+    printData(albums, users);
+
+    /* for (var i of response) {
+      console.log(`RESPONSE ITEM \n`);
+      for (var obj of i) {
+        console.log(obj); 
+        //logger utility method, logs output to screen
+      }
+    }*/
   } catch (error) {
     console.log(error);
   }
 }
-/* async function onRequest() {
-  try {
-    const response = await Promise.all([
-      fetch("https://jsonplaceholder.typicode.com/albums").then((response) =>
-        response.json()
-      ),
 
-      fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
-        response.json()
-      ),
-    ]);
-    for (var i of response) {
-      console.log(`RESPONSE ITEM \n`);
-      for (var obj of i) {
-        console.log(obj);
-        //logger utility method, logs output to screen
-      }
-    }
-  } catch (error) {
-    console.log(error);
-  } */
 /*     if (!response.ok) {
       const message = `Error: ${response.status}`;
       throw new Error(message);
@@ -59,34 +50,73 @@ async function onRequest() {
   } 
 }*/
 
-//let userTemporal = [];
-/* function printName(users2) {
-  const container = document.getElementById("container");
-  for (const user2 of users2) {
-    const pNombre = document.createElement("p");
-    const div = document.createElement("div");
-
-    pNombre.textContent = user2.name;
-
-    div.appendChild(pNombre);
-    container.appendChild(div);
-  }
-} */
-function printData(albums) {
+function printData(albums, users) {
   const container = document.getElementById("container");
 
   for (const album of albums) {
+    const user = users.find((user) => user.id == album.userId);
+
     const p = document.createElement("h2");
-    //const pNombre = document.createElement("p");
+    const pNombre = document.createElement("p");
     const div = document.createElement("div");
     const a = document.createElement("a");
     a.href = `photos.html?albumId=${album.id}`;
     p.textContent = album.title;
-    //pNombre.textContent = user.name;
+    pNombre.textContent = user.name;
 
-    //div.appendChild(pNombre);
+    div.appendChild(pNombre);
+
     a.appendChild(p);
     div.appendChild(a);
     container.appendChild(div);
   }
 }
+
+/* async function requestAlbums(users) {
+  try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/albums');
+      if (!response.ok) {
+          const message = `Error: ${response.status}`;
+          throw new Error(message);
+      }
+      const albums = await response.json();
+      showData(albums, users);
+  } catch (error) {
+      console.log(error)
+  }
+}
+
+async function requestUsers() {
+  try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      if (!response.ok) {
+          const message = `Error: ${response.status}`;
+          throw new Error(message);
+      }
+      const users = await response.json();
+      requestAlbums(users)
+  } catch (error) {
+      console.log(error)
+  }
+}
+
+requestUsers()
+
+function showData(albums, users) {
+  albums.sort(() => Math.random() - 0.5);
+  const lista = document.getElementById("lista")
+  for (const album of albums) {
+
+      const user = users.find(newUser => newUser.id == album.userId);
+
+      const title = document.createElement("h2")
+      const userName = document.createElement("p")
+
+      title.textContent = album.title
+      userName.textContent = user.name
+
+      lista.appendChild(title)
+      lista.appendChild(userName)
+  }
+}
+ */
